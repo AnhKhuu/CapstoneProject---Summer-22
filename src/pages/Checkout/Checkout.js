fb20e5cfad4ee0f55b5391ba88444fd33481f643
AnhKhuu <anhkhuu.3029@gmail.com>
2022-07-27 09:14:51 +0700

    create AdminLayout

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MainLayout from '../../layout/MainLayout';
+import { ADD_PRODUCT } from '../../graphql/mutations';
 
 const Checkout = () => {
   return <MainLayout>Checkout Page</MainLayout>;
